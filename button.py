847ba306097c2d39111d44818eb12db538a03652
kseniaglivko <k.glivko@mail.ru>
2021-11-25 23:04:41 +0300

    Minor refactoring, updated README.

--- button.py
+++ button.py
@@ -1,31 +1,29 @@
 import pygame
 
-from scoreboard import Scoreboard
 
 class Button:
-	
-	def __init__(self, ai_game, effect):
-		'''Initialising button attributes'''
-		self.screen = ai_game.screen
-		self.screen_rect = self.screen.get_rect()
-		
-		if effect == "play":
-			self.image = pygame.image.load('images/play_button.png')
-		#Sound button - by default it is a mute button image
-		#As the sound is on and mute button is for turning the sound off
-		elif effect == "sound":
-			self.image = pygame.image.load('images/mute_button.png')
-			
-		self.rect = self.image.get_rect()
-		
-		#Button placement
-		if effect == "play":
-			self.rect.center = self.screen_rect.center
-		elif effect == "sound":
-			self.rect = self.image.get_rect()
-			self.rect.left = self.screen_rect.left + 10
-			self.rect.top = 120
-		
-	def draw_button(self):
-		'''Drawing play button on the screen'''
-		self.screen.blit(self.image, self.rect)
+    def __init__(self, ai_game, effect):
+        """Initialising button attributes"""
+        self.screen = ai_game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        if effect == "play":
+            self.image = pygame.image.load("images/play_button.png")
+        # Sound button - by default it is a mute button image
+        # As the sound is on and mute button is for turning the sound off
+        elif effect == "sound":
+            self.image = pygame.image.load("images/mute_button.png")
+
+        self.rect = self.image.get_rect()
+
+        # Button placement
+        if effect == "play":
+            self.rect.center = self.screen_rect.center
+        elif effect == "sound":
+            self.rect = self.image.get_rect()
+            self.rect.left = self.screen_rect.left + 10
+            self.rect.top = 120
+
+    def draw_button(self):
+        """Drawing play button on the screen"""
+        self.screen.blit(self.image, self.rect)
---
+++ main.py
@@ -0,0 +1,6 @@
+from alien_invasion import AlienInvasion
+
+if __name__ == "__main__":
+    """Creating game object and launch"""
+    ai = AlienInvasion()
+    ai.run_game()
